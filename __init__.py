eb78cdd348e68dc7a897d25249bd9ac30e948015
Mirko Hirsch <root@smarthome.local>
2013-11-19 20:13:37 +0000

    cleanup and raw-mode

--- __init__.py
+++ __init__.py
@@ -16,16 +16,16 @@ cmd_dict=dict(
     stop = [137,0,0,0,0],
     forward = [137,0,100,128,0],
     backward = [137,255,156,128,0], 
-    spin_left = [137,0,100,255,255],
-    spin_right = [137,0,100,0,1],
+    spin_right = [137,0,100,255,255],
+    spin_left = [137,0,100,0,1],
     dock = 143,                 #change2passive
 )
 
-#support later
-#    motors = 138,               #add 1 databyte
-#    leds = 139,                 #add 3 databytes
-#    song = 140,                 #add 2n + 2 databytes
-#    play = 141,                 #add 1 databyte
+# maybe supported later
+# motors = 138,               #add 1 databyte
+# leds = 139,                 #add 3 databytes
+# song = 140,                 #add 2n + 2 databytes
+# play = 141,                 #add 1 databyte
 
 
 class Roomba(object):
@@ -35,14 +35,23 @@ class Roomba(object):
         self._sh = smarthome
         self.tty = tty
         self.baudrate = baudrate
-        self.ser = serial.Serial(self.tty, baudrate=self.baudrate, timeout=2)
+        try:
+            self.ser = serial.Serial(self.tty, baudrate=self.baudrate, timeout=2)
+        except:
+            pass
         self.is_connected = 'False'
         self._cycle = int(cycle)
         if self._cycle > 0:
-            self._sh.scheduler.add('Roomba', self.get_sensors, prio=5, cycle=self._cycle, offset=2)      
+            self._sh.scheduler.add('Roomba', self.get_sensors, prio=5, cycle=self._cycle, offset=10)      
     
     def run(self):
         pass
+    
+    def init_command(self):
+        self.send(128)  #start
+        time.sleep(0.2)
+        self.send(130)  #command
+        time.sleep(0.2)
         
     def send(self, raw):
         if self.is_connected == 'False':
@@ -54,22 +63,26 @@ class Roomba(object):
             if type(raw) is list:
                 #print ("Send {0}".format(raw))
                 logger.debug("Roomba: Send:{0}".format(raw))
-                self.ser.write(bytearray(raw))
+                try:
+                    self.ser.write(bytearray(raw))
+                except:
+                    logger.error("Roomba: Send failed for {}!".format([raw]))
+                    
             else:
                 #print ("Send [{0}]".format(raw))
                 logger.debug("Roomba: Send:{0}".format([raw]))
-                self.ser.write(bytearray([raw]))
+                try:
+                    self.ser.write(bytearray([raw]))
+                except:
+                    logger.error("Roomba: Send failed for {}!".format([raw]))
 
     def connect(self):
         logger.debug("Roomba: Try to connect")
         try:
             self.ser.open()
+            self.ser.flushInput()
             logger.debug("Roomba: Connected")
             self.is_connected = 'True'
-            #self.send(128)  #start
-            time.sleep(0.2)
-            self.send(130)  #command
-            time.sleep(0.2)
         except:
             logger.error("Roomba: Function connect failed")
             self.is_connected = 'False'
@@ -80,19 +93,14 @@ class Roomba(object):
         logger.debug("Roomba: Disconnected")
     
     def parse_item(self, item):
-        if 'roomba_cmd' in item.conf:
-            cmd_string = item.conf['roomba_cmd']
-            logger.debug("Roomba: {0} will send cmd \'{1}\'".format(item, cmd_string))
-            self._items.append(item)
-            return self.update_item
         if 'roomba_get' in item.conf:
             sensor_string = item.conf['roomba_get']
             logger.debug("Roomba: {0} will get \'{1}\'".format(item, sensor_string))
             self._items.append(item)
             return self.update_item      
-        if 'roomba_drive' in item.conf:
-            drive_string = item.conf['roomba_drive']
-            logger.debug("Roomba: {0} will drive \'{1}\'".format(item, drive_string))
+        if 'roomba_cmd' in item.conf:
+            cmd_string = item.conf['roomba_cmd']
+            logger.debug("Roomba: {0} will drive \'{1}\'".format(item, cmd_string))
             self._items.append(item)
             return self.update_item
         if 'roomba_raw' in item.conf:
@@ -102,23 +110,20 @@ class Roomba(object):
             return self.update_item	
             
     def update_item(self, item, caller=None, source=None, dest=None):
-        if caller != 'roomba':
+        if caller != 'Roomba':
             if item():
-                if 'roomba_cmd' in item.conf:	
+                if 'roomba_cmd' in item.conf:
                     cmd_string = item.conf['roomba_cmd']
                     logger.debug("Roomba: item = true")
-                    raw = cmd_dict[cmd_string]
-                    if cmd_string in cmd_dict:
-                        self.send(raw)
-                if 'roomba_drive' in item.conf:
-                    drive_string = item.conf['roomba_drive']
-                    logger.debug("Roomba: item = true")
-                    self.drive(drive_string)
+                    self.drive(cmd_string)
+                if 'roomba_raw' in item.conf:
+                    raw_string = item.conf['roomba_raw']
+                    self.raw(raw_string)
                 else:
                     pass
-                self.disconnect()
        
     def drive(self,cmd_string):
+        self.init_command()
         full_raw_cmd = []
         if type(cmd_string) is list:
             for i in cmd_string:
@@ -135,17 +140,19 @@ class Roomba(object):
         self.disconnect()
         
     def raw(self,raw_cmd):
-        pass
+        self.init_command()
+        self.send(self, raw)
+        self.disconnect()
         
     def get_sensors(self):
         self.send([142,0])
         answer = self.ser.read(26)
         if len(answer) == 26:
+            logger.debug("Roomba: Got sensor data.")
             answer = list(answer)
             #print (answer)
             
             #create sensor_dict
-            sensor_dict = {}
             sensor_dict = dict()
             
             #sensor_raw:
@@ -164,8 +171,9 @@ class Roomba(object):
             _voltage = self.DecodeUnsignedShort(answer[18],answer[17]) #voltage
             sensor_dict['voltage']=_voltage
             
-            _charging_state = self.DecodeUnsignedByte(answer[16]) #charging state
+            _charging_state = answer[16] #charging state
             sensor_dict['charging_state']=_charging_state
+            #0=Not Charging, 1=Charging Recovery, 2=Charging, 3=Trickle Charging, 4=Waiting, 5=Charging Error
             
             _angle = self.Angle(answer[15], answer[14], 'degrees') #angle
             sensor_dict['angle']=_angle
@@ -173,52 +181,55 @@ class Roomba(object):
             _distance = self.DecodeShort(answer[13],answer[12]) #distance
             sensor_dict['distance']=_distance
             
-            _buttons = list(self.Buttons(answer[11])) #Button
-            sensor_dict['buttons_max']=_buttons[0]
-            sensor_dict['buttons_clean']=_buttons[1]
-            sensor_dict['buttons_spot']=_buttons[2]
-            sensor_dict['buttons_power']=_buttons[3]
+            _buttons = answer[11] #Button
+            #returns max,clean,spot,power
+            sensor_dict['buttons_max']=bool(_buttons & 0x01)
+            sensor_dict['buttons_clean']=bool(_buttons & 0x02)
+            sensor_dict['buttons_spot']=bool(_buttons & 0x04)
+            sensor_dict['buttons_power']=bool(_buttons & 0x08)
             
-            _remote_opcode = self.DecodeUnsignedByte(answer[10]) #remote_opcode
+            _remote_opcode = answer[10] #remote_opcode
             sensor_dict['remote_opcode']=_remote_opcode
             
-            _dirt_detect_right = self.DecodeUnsignedByte(answer[9]) #dirt detect right
+            _dirt_detect_right = bool(answer[9]) #dirt detect right
             sensor_dict['dirt_detect_right']=_dirt_detect_right
             
-            _dirt_detect_left = self.DecodeUnsignedByte(answer[8]) #dirt detect left
+            _dirt_detect_left = bool(answer[8]) #dirt detect left
             sensor_dict['dirt_detect_left']=_dirt_detect_left
             
-            _motor_overcurrent = self.MotorOvercurrents(answer[7]) #motor overcurrent
-            sensor_dict['motor_overcurrent_side_brush']=_motor_overcurrent[0]
-            sensor_dict['motor_overcurrent_vacuum']=_motor_overcurrent[1]
-            sensor_dict['motor_overcurrent_main_brush']=_motor_overcurrent[2]
-            sensor_dict['motor_overcurrent_drive_right']=_motor_overcurrent[3]        
-            sensor_dict['motor_overcurrent_drive_left']=_motor_overcurrent[3] 
+            _motor_overcurrent = answer[7] #motor overcurrent
+            #returns side-brush,vacuum,main-brush,drive-right,drive-left
+            sensor_dict['motor_overcurrent_side_brush']=bool(_motor_overcurrent & 0x01)
+            sensor_dict['motor_overcurrent_vacuum']=bool(_motor_overcurrent & 0x02)
+            sensor_dict['motor_overcurrent_main_brush']=bool(_motor_overcurrent & 0x04)
+            sensor_dict['motor_overcurrent_drive_right']=bool(_motor_overcurrent & 0x08)        
+            sensor_dict['motor_overcurrent_drive_left']=bool(_motor_overcurrent & 0x010)
             
             _virtual_wall = answer[6]
-            sensor_dict['virtual_wall']=_virtual_wall
+            sensor_dict['virtual_wall']=bool(_virtual_wall)
             
             _cliff_right = answer[5]
-            sensor_dict['cliff_right']=_cliff_right
+            sensor_dict['cliff_right']=bool(_cliff_right)
             
             _cliff_front_right = answer[4]
-            sensor_dict['cliff_front_right']=_cliff_front_right
+            sensor_dict['cliff_front_right']=bool(_cliff_front_right)
             
             _cliff_front_left = answer[3]
-            sensor_dict['cliff_front_left']=_cliff_front_left
+            sensor_dict['cliff_front_left']=bool(_cliff_front_left)
             
             _cliff_left = answer[2]
-            sensor_dict['cliff_left']=_cliff_left
+            sensor_dict['cliff_left']=bool(_cliff_left)
             
             _wall = answer[1]
-            sensor_dict['wall']=_wall
+            sensor_dict['wall']=bool(_wall)
             
-            _bumps_wheeldrops = self.BumpsWheeldrops(answer[0])
-            sensor_dict['bumps_wheeldrops_bump_right']=_bumps_wheeldrops[0]
-            sensor_dict['bumps_wheeldrops_bump_left']=_bumps_wheeldrops[1]
-            sensor_dict['bumps_wheeldrops_wheeldrop_right']=_bumps_wheeldrops[2]
-            sensor_dict['bumps_wheeldrops_wheeldrop_left']=_bumps_wheeldrops[3]
-            sensor_dict['bumps_wheeldrops_wheeldrop_caster']=_bumps_wheeldrops[4]
+            _bumps_wheeldrops = answer[0]
+            #returns bump-right,bump-left,wheel-drop-right,wheel-drop-left,wheel-drop-caster
+            sensor_dict['bumps_wheeldrops_bump_right']=bool(_bumps_wheeldrops & 0x01) #bit 1
+            sensor_dict['bumps_wheeldrops_bump_left']=bool(_bumps_wheeldrops & 0x02) #bit 2
+            sensor_dict['bumps_wheeldrops_wheeldrop_right']=bool(_bumps_wheeldrops & 0x04) #bit 3
+            sensor_dict['bumps_wheeldrops_wheeldrop_left']=bool(_bumps_wheeldrops & 0x08) #bit 4
+            sensor_dict['bumps_wheeldrops_wheeldrop_caster']=bool(_bumps_wheeldrops & 0x10) #bit 5
             
             for item in self._items:
                 if 'roomba_get' in item.conf:
@@ -230,10 +241,6 @@ class Roomba(object):
         else:
             logger.error("Roomba: Sensors not readable")
         self.disconnect()
-        
-    def dec2hex(self,num):
-        num = int(num)
-        return (str('0x' + '%.2x' % num))
 
     def DecodeUnsignedShort(self, low, high):
         bytearr = bytearray([high,low])
@@ -250,11 +257,6 @@ class Roomba(object):
         value = unpack('>h', bytearr)
         return (value[0])
     
-    def DecodeUnsignedByte(self, byte):
-        bytearr = bytearray([byte])
-        value = unpack('B', bytearr)
-        return (value[0])
-    
     def Angle(self, low, high, unit=None):
         #Angle in radians = (2 * difference) / 258
         #Angle in degrees = (360 * difference) / (258 * Pi).
@@ -269,24 +271,6 @@ class Roomba(object):
             angle /= math.pi
             #print ("{0} degrees".format(angle))
             return angle
-    
-    def Buttons(self, byte):
-        bytearr = bytearray([byte])
-        byte = unpack('B', bytearr)
-        byte = byte[0]
-        return (byte & 1, byte & 2, byte & 3, byte & 4)
-        #returns max,clean,spot,power
-    
-    def MotorOvercurrents(self, byte):
-        bytearr = bytearray([byte])
-        byte = unpack('B', bytearr)
-        byte = byte[0]
-        return (byte & 1, byte & 2, byte & 3, byte & 4, byte & 5)
-        #returns side-brush,vacuum,main-brush,drive-right,drive-left
-    
-    def BumpsWheeldrops(self, byte):
-        bytearr = bytearray([byte])
-        byte = unpack('B', bytearr)
-        byte = byte[0]
-        return (byte & 1, byte & 2, byte & 3, byte & 4, byte & 5)
-        #returns bump-right,bump-left,wheel-drop-right,wheel-drop-left,wheel-drop-caster
+        
+
+        
